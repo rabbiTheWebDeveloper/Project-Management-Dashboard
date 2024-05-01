09da4ba1f92e19f8fc939e93169c3ac3504680ef
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-05-01 18:42:22 +0600

    Update TaskAction.jsx

--- src/component/Task/TaskAction.jsx
+++ src/component/Task/TaskAction.jsx
@@ -1,3 +1,5 @@
+"use client";
+import Link from 'next/link';
 import React from 'react';
 import { CiEdit } from 'react-icons/ci';
 import { FaEye } from 'react-icons/fa';
@@ -7,12 +9,12 @@ const TaskAction = () => {
   return (
     <td className="text-base font-normal text-custom-text-color2 p-5">
             <div className="flex items-center gap-2 justify-center">
-              <div className="bg-gray-100 p-2 inline-block rounded-md cursor-pointer">
+              <Link href="/task/details/1" className="bg-gray-100 p-2 inline-block rounded-md cursor-pointer" >
                 <FaEye />
-              </div>
-              <div className="bg-gray-100 p-2 inline-block rounded-md cursor-pointer">
+              </Link>
+              <Link href="/task/details/1" className="bg-gray-100 p-2 inline-block rounded-md cursor-pointer">
                 <CiEdit />
-              </div>
+              </Link>
               <div className="bg-gray-100 p-2 inline-block rounded-md cursor-pointer">
                 <MdDelete />
               </div>
